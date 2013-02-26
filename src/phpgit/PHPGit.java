82b236b5b831079b38e710c54eca76fb1cbd22e8
reget.kalamees <reget.kalamees@K8-OPETAJA.ametikool.local>
2013-02-26 15:06:55 +0200

    algus

---
+++ src/phpgit/PHPGit.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package phpgit;
+
+/**
+ *
+ * @author reget.kalamees
+ * 1. Võimalikult puhas projekt
+ * 2. git ignore ja exclude from commit (nbproject jne.)
+ * 3. kustuta ära sama nimega githubis olevad failid 
+ * 4. commit
+ * 5. pull (sel juhul kui repos on juba midagi)
+ * 6. push
+ * 
+ * Kui pull läheb katki - kõik käib läbi uute commitide
+ * 
+ */
+public class PHPGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
